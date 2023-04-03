3fa59e676435c7db400424bbbd3e00d2b7d9a872
Yasir_Hassan <yasir.hassan7722@gmail.com>
2023-04-03 15:18:48 -0500

    initial commit

---
+++ springboot-kafka-project/kafka-consumer-database/src/main/java/net/kafka_project/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package net.kafka_project.springboot.repository;
+
+import net.javaguides.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
